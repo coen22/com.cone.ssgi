7ed953d8c07d5d12f882337c24cbd14cec82d30e
Coen Hacking <coenhacking@cadac.com>
2025-10-13 17:14:13 +0200

    Fixes

--- Editor/ScreenSpaceGlobalIlluminationVolumeEditor.cs
+++ Editor/ScreenSpaceGlobalIlluminationVolumeEditor.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using Cone.SSGI;
+using Cone.SSGI.Denoisers;
 using UnityEditor;
 using UnityEditor.Rendering;
 using UnityEngine;
