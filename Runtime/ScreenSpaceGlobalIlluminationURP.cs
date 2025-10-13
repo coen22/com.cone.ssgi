b5311c735b515045018d279093e48aef191c53d5
Coen Hacking <coen.hacking@gmail.com>
2025-10-13 17:06:58 +0200

    Close namespaces

--- Runtime/ScreenSpaceGlobalIlluminationURP.cs
+++ Runtime/ScreenSpaceGlobalIlluminationURP.cs
@@ -4754,3 +4754,4 @@ public class ScreenSpaceGlobalIlluminationURP : ScriptableRendererFeature
         #endregion
     }
 }
+}
--- Runtime/SpatiotemporalBlueNoise.cs
+++ Runtime/SpatiotemporalBlueNoise.cs
@@ -183,3 +183,4 @@ internal static class SpatiotemporalBlueNoise
         }
     }
 }
+}
