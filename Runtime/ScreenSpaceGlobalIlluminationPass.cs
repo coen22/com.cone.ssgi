f4026e8075d96a7d014bef09d46fad565b2e7488
Coen Hacking <coen.hacking@gmail.com>
2025-10-14 10:56:58 +0200

    Skip null reflection probes when selecting fallback

--- Runtime/ScreenSpaceGlobalIlluminationPass.cs
+++ Runtime/ScreenSpaceGlobalIlluminationPass.cs
@@ -3189,6 +3189,10 @@ namespace Cone.SSGI
             foreach (var visibleProbe in visibleReflectionProbes)
             {
                 ReflectionProbe probe = visibleProbe.reflectionProbe;
+                if (probe == null)
+                {
+                    continue;
+                }
                 Bounds probeBounds = probe.bounds;
                 int probeImportance = probe.importance;
                 float boundsSizeSqr = probeBounds.size.sqrMagnitude;
