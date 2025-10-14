14c433130a7e27883785e650476b4eb366f81448
Coen Hacking <coen.hacking@gmail.com>
2025-10-14 17:40:16 +0200

    Guard reverse bits helper

--- Shaders/SSGIUtilities.hlsl
+++ Shaders/SSGIUtilities.hlsl
@@ -61,7 +61,8 @@ float2 SampleScreenBlueNoise(float2 pixelCoord, uint frameIndex, uint sampleInde
     return float2(Hash31(seed0), Hash31(seed1));
 }
 
-uint SSGIReverseBits32(uint bits)
+#ifndef ReverseBits32
+uint ReverseBits32(uint bits)
 {
     bits = ((bits & 0x55555555u) << 1) | ((bits & 0xAAAAAAAAu) >> 1);
     bits = ((bits & 0x33333333u) << 2) | ((bits & 0xCCCCCCCCu) >> 2);
@@ -69,6 +70,12 @@ uint SSGIReverseBits32(uint bits)
     bits = ((bits & 0x00FF00FFu) << 8) | ((bits & 0xFF00FF00u) >> 8);
     bits = (bits << 16) | (bits >> 16);
     return bits;
+}
+#endif
+
+uint SSGIReverseBits32(uint bits)
+{
+    return ReverseBits32(bits);
 }
 
 uint LowBiasHash(uint x)
